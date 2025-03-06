cb03a283e96b1b09eb2e68d79b57fbe9ed0d5d61
Mz-scripter <mzscripterx5@gmail.com>
2025-03-06 13:49:43 +0100

    Added basic chat functionality.

--- chats/views.py
+++ chats/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import ChatRoom
 from django.contrib.auth.models import User
+from .forms import MessageForm
+from django.http import HttpResponseForbidden
 
 
 @login_required
@@ -28,4 +30,27 @@ def chat_list(request):
 @login_required
 def chat_detail(request, chat_room_id):
     chat_room = ChatRoom.objects.get(id=chat_room_id)
-    return render(request, 'chats/chat_detail.html', {'chat_room': chat_room})
+    
+    if request.user not in chat_room.participants.all():
+        return HttpResponseForbidden("You are not a participant in this chat room.")
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.chat_room = chat_room
+            message.sender = request.user
+            if not chat_room.is_group_chat:
+                message.is_anonymous = False
+            message.save()
+            return redirect('chat_detail', chat_room_id=chat_room.id)
+    else:
+        form = MessageForm()
+    
+    messages = chat_room.messages.order_by('timestamp')
+    context = {
+        'chat_room': chat_room,
+        'messages': messages,
+        'form': form,
+    }
+    return render(request, 'chats/chat_detail.html', context)
