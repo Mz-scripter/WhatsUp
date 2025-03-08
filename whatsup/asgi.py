ab47a758778140e1662fc3aef2242a727931b6c7
Mz-scripter <mzscripterx5@gmail.com>
2025-03-08 09:02:22 +0100

    Installed Redis and implemented realtime messaging.

--- whatsup/asgi.py
+++ whatsup/asgi.py
@@ -1,13 +1,24 @@
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-import chats.routing
+from dotenv import load_dotenv
+from pathlib import Path
+import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'whatsup.settings')
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": URLRouter(
-        chats.routing.websocket_urlpatterns
-    ),
-})
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv("DJANGO_SETTINGS_MODULE", "whatsup.settings"))
+
+django.setup()
+
+def get_asgi_app():
+    from channels.routing import ProtocolTypeRouter, URLRouter
+    from chats import routing
+    return ProtocolTypeRouter({
+        "http": get_asgi_application(),
+        "websocket": URLRouter(
+            routing.websocket_urlpatterns
+        ),
+    })
+    
+application = get_asgi_app()
+
